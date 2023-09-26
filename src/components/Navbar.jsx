ffd87770b93f92bb013c45fc9cf39b6232770732
praneeth <kodadalajp0503@gmail.com>
2023-09-27 01:53:23 +0530

    fifth commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ function Navbar() {
         <header className="bg-black">
             <nav className="flex justify-between items-center h-16 sticky text-white z-10">
                 <div className="logo text-xl mx-4"><Link to="/">TubeToday</Link></div>
-                <div className="menu hidden md:block"><FontAwesomeIcon icon={faBars} onClick={onToggle} className="cursor-pointer mx-4 "/></div>
+                <div className="menu hidden md:block"><FontAwesomeIcon icon={!isNavOpen ?faBars:faX} onClick={onToggle} className="cursor-pointer mx-4 "/></div>
                 <div className={`md:absolute md:bg-black md:min-h-[60vh] min-h-fit left:0 md:w-full md:z-[-1] md:flex md:items-center ${isNavOpen ?'top-[9%]':'top-[-400px]'}`}>
                     <ul className="flex mx-6 md:flex-col md:m-auto md:space-y-8">
                         <li className="mr-10 md:mr-0"><Link className="hover:text-red-500 active:text-gray-400 " onClick={closeNavbar} to="/">Upload a video</Link></li>
